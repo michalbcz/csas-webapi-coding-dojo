386104b279be8d28a9ce3052b3c3da42ff37a3fe
Karel Stefan <karel.stefan@topmonks.com>
2017-03-21 17:17:03 +0100

    Add skeleton for coding dojo #3

---
+++ coding-dojo-3/src/main/groovy/cz/csas/webapi/codingdojo/StringCalculator.groovy
@@ -0,0 +1,9 @@
+package cz.csas.webapi.codingdojo
+
+class StringCalculator {
+
+    static def int add(String input) {
+        throw new UnsupportedOperationException("Not implemented yet")
+    }
+
+}
